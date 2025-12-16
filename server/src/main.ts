51b6c5bef0758fc9951d7741f262ed642687808c
MATHIEU HAMMEL <mtahammel@gmail.com>
2025-12-16 11:17:23 +0100

    Exercise done

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,18 +1,17 @@
-// Load environment variables from .env file
 import "dotenv/config";
-
-// Check database connection
-// Note: This is optional and can be removed if the database connection
-// is not required when starting the application
 import "../database/checkConnection";
-
-// Import the Express application from ./app
 import app from "./app";
 
-// Get the port from the environment variables
-const port = process.env.APP_PORT;
+import type { RequestHandler } from "express";
 
-// Start the server and listen on the specified port
+const sayWelcome: RequestHandler = (req, res) => {
+  res.send("Welcome to Wild Series !");
+};
+
+app.get("/", sayWelcome);
+
+const port = process.env.APP_PORT || 3310;
+
 app
   .listen(port, () => {
     console.info(`Server is listening on port ${port}`);
